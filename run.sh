5c945ffa40bab07c6b45e0fcafa0134cd191cb0d
Victor Gladkikh <me@alfeg.net>
2020-09-28 14:14:03 +0300

    Provide email for lets encrypt

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker-compose up -d
+
+export $(grep -v '#.*' .env | xargs)
+echo "Open browser at http://$HQ_HOST and finish HQ installation"
--- run.ssl.sh
+++ run.ssl.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml -f docker-compose.ssl.yml up -d
+
+export $(grep -v '#.*' .env | xargs)
+echo "Open browser at https://$HQ_HOST and finish HQ installation"
